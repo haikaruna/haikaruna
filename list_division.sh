0f87321382b96742309d8bb91307367530fa21e9
haikaruna <49455793+haikaruna@users.noreply.github.com>
2021-10-25 21:54:58 +0530

    Update list_division.sh

--- list_division.sh
+++ list_division.sh
@@ -1,7 +1,11 @@
-h=$(cat list | wc -l)
-n=1
+######### list is a filename which contains n number of lines with each line having some values ########
+######### Below program will create files with 10 values each depends on number of lines in the list file.
+######### Example if the list file has contents from 1 to 25 on each line, then below program will create 3 files list_1,list_2,list_3 having values 1 to 10, 11 to 20 and 21 to 25 respectively.
+
+h=$(cat list | wc -l)  ###Getting number of lines in the file
+
 echo " Number of lines in list is $h"
-list_num=$((h/10))
+list_num=$((h/10))    ####Finding number of batched to be created
 
 echo "Number of files to be created $list_num"
 
